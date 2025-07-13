1143f48d17b901445631d34329902f67afb00481
Tore Aurstad <tore.aurstad@gmail.com>
2025-07-13 23:33:07 +0200

    Added image upload the blazor serverside app

---
+++ ObjectDetectionMachineLearning.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace ObjectDetectionMachineLearning.Web.Components.Pages
+{
+    partial class Home
+    {
+
+        private string? UploadedImagePreview;
+
+        /// <summary>
+        /// Uploads an image and sets the imagePreview property to display it
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private async Task OnInputFile(InputFileChangeEventArgs e)
+        {
+            var file = e.File;
+
+            if (file != null && (file!.ContentType == "image/jpeg" || file!.ContentType == "image/png"))
+            {
+                using (var stream = file.OpenReadStream(maxAllowedSize: 10 * 1024 * 1024))
+                {
+                    using var ms = new MemoryStream();
+                    await stream.CopyToAsync(ms);
+                    var bytes = ms.ToArray();
+                    UploadedImagePreview = $"data:{file.ContentType};base64,{Convert.ToBase64String(bytes)}";
+                }
+            }
+        }
+
+    }
+}
